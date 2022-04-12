13a343572341fad1dc690ea6fb56db0a528bfacf
HuskyGT <103238785+HuskyGT@users.noreply.github.com>
2022-04-12 16:10:50 -0400

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using BepInEx;
+using UnityEngine;
+using UnityEngine.XR;
+using Utilla;
+using InputDevice = UnityEngine.InputSystem.InputDevice;
+
+
+
+
+
+namespace UpAndDown
+{
+    /// <summary>
+    /// This is your mod's main class.
+    /// </summary>
+
+    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+    [Description("HauntedModMenu")]
+	[ModdedGamemode]
+	[BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class UpAndDown : BaseUnityPlugin
+	{
+		bool inRoom;
+        static bool Up;
+		static bool Down;
+
+        void OnEnable() {
+			/* Set up your mod here */
+			/* Code here runs at the start and whenever your mod is enabled*/
+
+
+			HarmonyPatches.ApplyHarmonyPatches();
+			Utilla.Events.GameInitialized += OnGameInitialized;
+		}
+
+		void OnDisable() {
+			/* Undo mod setup here */
+			/* This provides support for toggling mods with ComputerInterface, please implement it :) */
+			/* Code here runs whenever your mod is disabled (including if it disabled on startup)*/
+
+			HarmonyPatches.RemoveHarmonyPatches();
+			Utilla.Events.GameInitialized -= OnGameInitialized;
+		}
+
+		void OnGameInitialized(object sender, EventArgs e)
+		{
+			/* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
+		}
+
+		void Update()
+		{
+            if (inRoom)
+            {
+
+                /* Code here runs every frame when the mod is enabled */
+                /* i hate people that use airjump to jail people */
+                
+                InputDevices.GetDeviceAtXRNode(XRNode.RightHand).TryGetFeatureValue(CommonUsages.triggerButton, out Up);
+
+                if (Up)
+                {
+                    GorillaLocomotion.Player.Instance.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 1000f, 0f), ForceMode.Acceleration);
+                }
+				
+				InputDevices.GetDeviceAtXRNode(XRNode.LeftHand).TryGetFeatureValue(CommonUsages.triggerButton, out Down);
+
+				if (Down)
+				{
+					GorillaLocomotion.Player.Instance.GetComponent<Rigidbody>().AddForce(new Vector3(0f, -1000f, 0f), ForceMode.Acceleration);
+				}
+			}
+        }
+
+		/* This attribute tells Utilla to call this method when a modded room is joined */
+		[ModdedGamemodeJoin]
+		public void OnJoin(string gamemode)
+		{
+			/* Activate your mod here */
+			/* This code will run regardless of if the mod is enabled*/
+
+			inRoom = true;
+		}
+
+		/* This attribute tells Utilla to call this method when a modded room is left */
+		[ModdedGamemodeLeave]
+		public void OnLeave(string gamemode)
+		{
+			/* Deactivate your mod here */
+			/* This code will run regardless of if the mod is enabled*/
+
+			inRoom = false;
+		}
+	}
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace UpAndDown
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.AuthorLowerName.gorillatag.GorillaTagLowerModTemplateProject";
+		public const string Name = "UpAndDown";
+		public const string Version = "1.0.0";
+	}
+}
