ae91399c9311dd21b54d382df3ad1189db28abf9
HuskyGT <103238785+HuskyGT@users.noreply.github.com>
2022-06-13 03:37:35 -0400

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -1,12 +1,12 @@
-﻿namespace UpAndDown
+namespace UpAndDown
 {
 	/// <summary>
 	/// This class is used to provide information about your mod to BepInEx.
 	/// </summary>
 	class PluginInfo
 	{
-		public const string GUID = "com.AuthorLowerName.gorillatag.GorillaTagLowerModTemplateProject";
+		public const string GUID = "com.Husky.gorillatag.UpAndDown";
 		public const string Name = "UpAndDown";
-		public const string Version = "1.0.0";
+		public const string Version = "1.0.1";
 	}
 }
